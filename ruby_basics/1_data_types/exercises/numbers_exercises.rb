d685378b25620352ef0b2dcc136d0b37c7a246f2
HamnYce <hamadboa@gmail.com>
2021-09-07 02:19:33 +0300

    solved 06_array_exercises

--- ruby_basics/1_data_types/exercises/numbers_exercises.rb
+++ ruby_basics/1_data_types/exercises/numbers_exercises.rb
@@ -1,26 +1,28 @@
+# frozen_string_literal: true
+
 def add(a, b)
   # return the result of adding a and b
-  return a + b
+  a + b
 end
 
 def subtract(a, b)
-  return a - b
+  a - b
 end
 
 def multiply(a, b)
-  return a * b
+  a * b
 end
 
 def divide(a, b)
-  a/b
+  a / b
 end
 
 def remainder(a, b)
-  return a%b
-  end
+  a % b
+end
 
 def float_division(a, b)
-  a/(b.to_f)
+  a / b.to_f
 end
 
 def string_to_number(string)
